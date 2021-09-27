ace964237cfb2aee2dc234c61b469977c2b696d1
Anders Gustav Kensby <Anders.Gustav.Kensby@dnb.no>
2021-09-27 16:10:37 +0200

    first working commit

--- main.go
+++ main.go
@@ -2,13 +2,79 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli"
 )
 
+func getAllFeatures(c *gin.Context) {
+	c.String(http.StatusOK, "TODO: features")
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	input := &dynamodb.ListTablesInput{}
+
+fmt.Printf("Tables:\n")
+
+for {
+    // Get the list of tables
+    result, err := svc.ListTables(input)
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok {
+            switch aerr.Code() {
+            case dynamodb.ErrCodeInternalServerError:
+                fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+            default:
+                fmt.Println(aerr.Error())
+            }
+        } else {
+            // Print the error, cast err to awserr.Error to get the Code and
+            // Message from an error.
+            fmt.Println(err.Error())
+        }
+        return
+    }
+
+    for _, n := range result.TableNames {
+        fmt.Println(*n)
+    }
+
+    // assign the last read tablename as the start for our next call to the ListTables function
+    // the maximum number of table names returned in a call is 100 (default), which requires us to make
+    // multiple calls to the ListTables function to retrieve all table names
+    input.ExclusiveStartTableName = result.LastEvaluatedTableName
+
+    if result.LastEvaluatedTableName == nil {
+        break
+    }
+}
+}
+
+func getFeatureById(c *gin.Context) {
+	id := c.Query("id")
+	c.String(http.StatusOK, "TODO: getFeatureById, %s", id)
+}
+
 func StartServer(c *cli.Context) {
-	fmt.Println("TODO: Start server.")
+	router := gin.Default()
+
+	v0 := router.Group("/v0")
+	
+	v0.GET("features", getAllFeatures)
+	v0.GET("features/:id", getFeatureById)
+	
+
+	router.Run()
 }
 
 func Execute() {
